1dcfe099168e7547680ed739c052a9f7cf4db3bc
Jake Lee <github@jakelee.co.uk>
2018-10-14 19:10:12 +0100

    Live listening, deleting

--- app/src/main/java/uk/co/jakelee/firebasereference/develop/cloud_firestore/CloudFirestoreFragment.kt
+++ app/src/main/java/uk/co/jakelee/firebasereference/develop/cloud_firestore/CloudFirestoreFragment.kt
@@ -6,10 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_develop_cloud_firestore.*
 import uk.co.jakelee.firebasereference.BaseFirebaseFragment
 import uk.co.jakelee.firebasereference.R
+import java.text.SimpleDateFormat
 import java.util.*
 
 class CloudFirestoreFragment : BaseFirebaseFragment() {
@@ -29,81 +31,96 @@ class CloudFirestoreFragment : BaseFirebaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setOnclicks()
-        updateRowList()
+        db.collection(tableName).addSnapshotListener { querySnapshot, _ ->
+            displayDocuments(querySnapshot!!.documents)
+        }
     }
 
+
     private fun setOnclicks() {
-        updateButton.setOnClickListener { updateRowList() }
+        fetchButton.setOnClickListener { fetchRows() }
         addAutomaticButton.setOnClickListener { addImplicitDocument() }
         addManualButton.setOnClickListener { addExplicitDocument() }
-        editButton.setOnClickListener { updateRow() }
+        editButton.setOnClickListener { editAllRows() }
         deleteButton.setOnClickListener { deleteRow() }
     }
 
-    private fun updateRowList() {
-        db.collection(tableName)
+    private fun fetchRows() = db.collection(tableName)
                 .get()
                 .addOnCompleteListener { task ->
                     if (task.isSuccessful) {
-                        var text = "ID\tNumber\tAdded"
-                        for (document in task.result!!) {
-                            text += "${document.id}\t${document.data["number"]}\t${document.data["added"]}"
-                        }
-                        tableContents.text = text
+                        displayDocuments(task.result?.documents!!)
                     } else {
                         showToast("Error getting documents: ${task.exception}")
                         tableContents.text = ""
                     }
                 }
-    }
 
-    private fun addImplicitDocument() {
-        db.collection(tableName)
+    private fun addImplicitDocument() = db.collection(tableName)
                 .add(generateDocument())
                 .addOnSuccessListener { documentReference ->
                     showToast("DocumentSnapshot added with ID: ${documentReference.id}")
-                    updateRowList()
                 }
                 .addOnFailureListener { e ->
                     showToast("Error adding document: $e")
                 }
-    }
 
-    private var rowId = 0
     private fun addExplicitDocument() {
-        db.collection(tableName).document(rowId.toString())
+        val id = (1..100000).shuffled().first().toString()
+        db.collection(tableName).document(id)
                 .set(generateDocument())
                 .addOnSuccessListener {
-                    showToast("DocumentSnapshot added with ID: $rowId")
-                    updateRowList()
+                    showToast("DocumentSnapshot added with ID: $id")
                 }
                 .addOnFailureListener { e ->
                     showToast("Error writing document: $e")
                 }
-        rowId++
     }
 
-    private fun updateRow() {
-        // get row
-        // change row (add 1 to number, change updated)
-    }
+    private fun editAllRows() = db.collection(tableName)
+                .get()
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        task.result!!.documents.forEach {
+                            val ref = db.collection(tableName).document(it.id)
+                            it.data?.let {
+                                ref.update("number", (it["number"] as Long) + 1)
+                            }
+                        }
+                    } else {
+                        showToast("Error getting documents: ${task.exception}")
+                        tableContents.text = ""
+                    }
+                }
 
-    private fun deleteRow() {
-        db.collection(tableName).document(0.toString())
-                .delete()
-                .addOnSuccessListener {
-                    showToast("DocumentSnapshot successfully deleted with ID: 0")
-                    updateRowList()
+    private fun deleteRow() = db.collection(tableName)
+                .get()
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        task.result!!.documents[0]?.let {
+                            val ref = db.collection(tableName).document(it.id)
+                            ref.delete()
+                            showToast("DocumentSnapshot successfully deleted with ID: ${ref.id}")
+                        }
+                    } else {
+                        showToast("Error getting documents: ${task.exception}")
+                        tableContents.text = ""
+                    }
                 }
-                .addOnFailureListener { e ->
-                    showToast("Error deleting document with ID 0: $e")
-                }
-    }
 
     private fun generateDocument(): HashMap<String, Any> {
+        val dateFormat = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
         return hashMapOf(
                 "number" to (1..100).shuffled().first(),
-                "added" to Calendar.getInstance().time.toString())
+                "added" to dateFormat.format(Calendar.getInstance().time))
+    }
+
+    private fun displayDocuments(t: List<DocumentSnapshot>) {
+        var text = ""
+        for (document in t) {
+            text += "ID ${document.id} has number ${document.data!!["number"]}\n"
+        }
+        tableContents.text = text
     }
 
     private fun showToast(text: String) {
