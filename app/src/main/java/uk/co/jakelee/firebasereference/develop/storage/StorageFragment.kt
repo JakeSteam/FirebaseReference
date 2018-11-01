2e53b97127a84f308ad1c9bc2ba34a00418f3d7e
Jake Lee <github@jakelee.co.uk>
2018-11-01 17:04:44 +0000

    Basic structure decided, untested

--- app/src/main/java/uk/co/jakelee/firebasereference/develop/storage/StorageFragment.kt
+++ app/src/main/java/uk/co/jakelee/firebasereference/develop/storage/StorageFragment.kt
@@ -1,12 +1,21 @@
 package uk.co.jakelee.firebasereference.develop.storage
 
 
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TableLayout
+import android.widget.Toast
 import uk.co.jakelee.firebasereference.BaseFirebaseFragment
 import uk.co.jakelee.firebasereference.R
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import kotlinx.android.synthetic.main.fragment_develop_storage.*
+
 
 class StorageFragment : BaseFirebaseFragment() {
     override val title = R.string.title_storage
@@ -20,5 +29,50 @@ class StorageFragment : BaseFirebaseFragment() {
         return inflater.inflate(R.layout.fragment_develop_storage, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val storage = FirebaseStorage.getInstance()
 
+        val sampleDirectory = storage.reference.child("sample")
+        val sampleFiles = arrayOf("Cat.png", "Dog.png", "Duck.png")
+        updateTable(sampleFilesTable, sampleDirectory, sampleFiles, false)
+
+        val userDirectory = storage.reference.child("userFolder").child(Build.FINGERPRINT)
+        // load user files from something like a shared pref
+        // updateTable(userFilesTable, userDirectory, userFiles, true)
+    }
+
+    private fun updateTable(table: TableLayout, sampleDirectory: StorageReference, files: Array<String>, canDelete: Boolean) {
+        for (file in files) {
+            val reference = sampleDirectory.child(file)
+            // inflate a row, and for the appropriate buttons set onclicks:
+            viewMetadata(reference)
+            downloadFile(reference)
+        }
+    }
+
+    private fun viewMetadata(reference: StorageReference) = reference.metadata
+            .addOnSuccessListener {
+                Toast.makeText(activity, "${it.name} has a size of ${it.sizeBytes} bytes, and is a ${it.contentType}", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {
+                Toast.makeText(activity, "Failed to load metadata: ${it.localizedMessage}", Toast.LENGTH_SHORT).show()
+            }
+
+    private fun downloadFile(reference: StorageReference) = reference.getBytes(10 * 1024 * 1024)
+            .addOnSuccessListener { bytes ->
+                activity!!.openFileOutput(reference.name, Context.MODE_PRIVATE).use {
+                    it.write(bytes)
+                }
+                Toast.makeText(activity, "Downloaded ${reference.name} from ${reference.downloadUrl}!", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {
+                Toast.makeText(activity, "Failed to download file: ${it.localizedMessage}", Toast.LENGTH_SHORT).show()
+            }
+
+    private fun uploadFile() {
+        // Display file picker
+        // Upload bytes
+        // Store reference somewhere, so that the user folder can display it
+    }
 }
