e71ed8c132dcb553a0d5c35b9e72655815253964
Jake Lee <github@jakelee.co.uk>
2018-11-06 23:09:22 +0000

    Basic layout, loading sample files

--- app/src/main/java/uk/co/jakelee/firebasereference/develop/storage/StorageFragment.kt
+++ app/src/main/java/uk/co/jakelee/firebasereference/develop/storage/StorageFragment.kt
@@ -2,19 +2,18 @@ package uk.co.jakelee.firebasereference.develop.storage
 
 
 import android.content.Context
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TableLayout
 import android.widget.Toast
-import uk.co.jakelee.firebasereference.BaseFirebaseFragment
-import uk.co.jakelee.firebasereference.R
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
+import kotlinx.android.synthetic.main.element_storage_row_sample.view.*
 import kotlinx.android.synthetic.main.fragment_develop_storage.*
+import uk.co.jakelee.firebasereference.BaseFirebaseFragment
+import uk.co.jakelee.firebasereference.R
 
 
 class StorageFragment : BaseFirebaseFragment() {
@@ -34,20 +33,23 @@ class StorageFragment : BaseFirebaseFragment() {
         val storage = FirebaseStorage.getInstance()
 
         val sampleDirectory = storage.reference.child("sample")
-        val sampleFiles = arrayOf("Cat.png", "Dog.png", "Duck.png")
+        val sampleFiles = arrayOf("Cat.jpg", "Dog.jpg", "Duck.jpg")
         updateTable(sampleFilesTable, sampleDirectory, sampleFiles, false)
 
-        val userDirectory = storage.reference.child("userFolder").child(Build.FINGERPRINT)
+        //val userDirectory = storage.reference.child("userFolder").child(Build.FINGERPRINT)
         // load user files from something like a shared pref
         // updateTable(userFilesTable, userDirectory, userFiles, true)
     }
 
     private fun updateTable(table: TableLayout, sampleDirectory: StorageReference, files: Array<String>, canDelete: Boolean) {
+        val inflater = LayoutInflater.from(activity!!)
         for (file in files) {
             val reference = sampleDirectory.child(file)
-            // inflate a row, and for the appropriate buttons set onclicks:
-            viewMetadata(reference)
-            downloadFile(reference)
+            val row = inflater.inflate(R.layout.element_storage_row_sample, table, false)
+            row.filename.text = reference.name
+            row.viewMetadataButton.setOnClickListener { viewMetadata(reference) }
+            row.downloadButton.setOnClickListener { downloadFile(reference) }
+            table.addView(row)
         }
     }
 
@@ -63,8 +65,8 @@ class StorageFragment : BaseFirebaseFragment() {
             .addOnSuccessListener { bytes ->
                 activity!!.openFileOutput(reference.name, Context.MODE_PRIVATE).use {
                     it.write(bytes)
+                    Toast.makeText(activity, "Downloaded ${reference.name} from ${it}!", Toast.LENGTH_SHORT).show()
                 }
-                Toast.makeText(activity, "Downloaded ${reference.name} from ${reference.downloadUrl}!", Toast.LENGTH_SHORT).show()
             }
             .addOnFailureListener {
                 Toast.makeText(activity, "Failed to download file: ${it.localizedMessage}", Toast.LENGTH_SHORT).show()
