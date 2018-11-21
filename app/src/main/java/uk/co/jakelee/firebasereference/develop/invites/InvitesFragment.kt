a6376174eee64e39d91d1098c9fbd85a8e39b5d4
Jake Lee <github@jakelee.co.uk>
2018-11-21 14:19:57 +0000

    Added broken invite button

--- app/src/main/java/uk/co/jakelee/firebasereference/develop/invites/InvitesFragment.kt
+++ app/src/main/java/uk/co/jakelee/firebasereference/develop/invites/InvitesFragment.kt
@@ -1,10 +1,13 @@
 package uk.co.jakelee.firebasereference.develop.invites
 
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.appinvite.AppInviteInvitation
+import kotlinx.android.synthetic.main.fragment_develop_invites.*
 import uk.co.jakelee.firebasereference.BaseFirebaseFragment
 import uk.co.jakelee.firebasereference.R
 
@@ -13,11 +16,25 @@ class InvitesFragment : BaseFirebaseFragment() {
     override val tutorialUrl = R.string.tutorial_invites
     override val docsUrl = R.string.documentation_invites
     override val firebaseUrl = R.string.firebase_invites
+    private val INVITE_REQUEST_CODE = 54321
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_develop_invites, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        sendInviteButton.setOnClickListener {
+            activity!!.startActivityForResult(
+                    AppInviteInvitation.IntentBuilder("title")
+                        .setMessage("message")
+                        .setDeepLink(Uri.parse("deep link"))
+                        .setCustomImage(Uri.parse("custom image url"))
+                        .setCallToActionText("call to action")
+                        .build(), INVITE_REQUEST_CODE)
+        }
     }
 
 
