5a1f6467dc8ca0fb488cfa1366280a3d7a99384d
Jake Lee <github@jakelee.co.uk>
2018-11-21 14:49:38 +0000

    All implemented, but untested

--- app/src/main/java/uk/co/jakelee/firebasereference/develop/invites/InvitesFragment.kt
+++ app/src/main/java/uk/co/jakelee/firebasereference/develop/invites/InvitesFragment.kt
@@ -1,12 +1,19 @@
 package uk.co.jakelee.firebasereference.develop.invites
 
 
+import android.app.Activity
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.google.android.gms.appinvite.AppInviteInvitation
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.appinvite.FirebaseAppInvite
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks
 import kotlinx.android.synthetic.main.fragment_develop_invites.*
 import uk.co.jakelee.firebasereference.BaseFirebaseFragment
 import uk.co.jakelee.firebasereference.R
@@ -26,16 +33,42 @@ class InvitesFragment : BaseFirebaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        sendInviteButton.setOnClickListener {
-            activity!!.startActivityForResult(
-                    AppInviteInvitation.IntentBuilder("title")
-                        .setMessage("message")
-                        .setDeepLink(Uri.parse("deep link"))
-                        .setCustomImage(Uri.parse("custom image url"))
-                        .setCallToActionText("call to action")
-                        .build(), INVITE_REQUEST_CODE)
-        }
+        processPendingInvite()
+        sendInviteButton.setOnClickListener { sendInviteOnClick() }
     }
 
+    private fun sendInviteOnClick() = activity!!
+            .startActivityForResult(
+                    AppInviteInvitation.IntentBuilder("title")
+                            .setMessage("custom message sent with invitations")
+                            .setDeepLink(Uri.parse("deep link to some kind of just-invited screen"))
+                            .setCustomImage(Uri.parse("url of square image"))
+                            .setCallToActionText("button text")
+                            .build(), INVITE_REQUEST_CODE)
 
+    private fun processPendingInvite() = FirebaseDynamicLinks.getInstance()
+            .getDynamicLink(activity!!.intent)
+            .addOnSuccessListener { data ->
+                if (data == null) {
+                    Log.d("Invites", "getInvitation: no data")
+                } else {
+                    val deepLink = data.link
+                    val inviteId = FirebaseAppInvite.getInvitation(data).invitationId
+                    Toast.makeText(activity!!, "Retrieved deep link of $deepLink and an inviteId of $inviteId", Toast.LENGTH_LONG).show()
+                }
+            }
+            .addOnFailureListener { Log.e("Invites", "Failed") }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == INVITE_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                val ids = AppInviteInvitation.getInvitationIds(resultCode, data!!)
+                val idsString = ids.joinToString()
+                Toast.makeText(activity!!, "Invited: $idsString", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(activity!!, "Invite sending failed / cancelled: $resultCode", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
 }
