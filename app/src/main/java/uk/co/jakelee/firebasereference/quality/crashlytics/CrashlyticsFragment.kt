c48060934d55639eaa4b15bb64cff4b5ba5a3f8c
Jake Lee <github@jakelee.co.uk>
2018-11-22 06:55:55 +0000

    Implemented

--- app/src/main/java/uk/co/jakelee/firebasereference/quality/crashlytics/CrashlyticsFragment.kt
+++ app/src/main/java/uk/co/jakelee/firebasereference/quality/crashlytics/CrashlyticsFragment.kt
@@ -1,10 +1,14 @@
 package uk.co.jakelee.firebasereference.quality.crashlytics
 
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.crashlytics.android.Crashlytics
+import kotlinx.android.synthetic.main.fragment_quality_crashlytics.*
 import uk.co.jakelee.firebasereference.BaseFirebaseFragment
 import uk.co.jakelee.firebasereference.R
 
@@ -20,5 +24,18 @@ class CrashlyticsFragment : BaseFirebaseFragment() {
         return inflater.inflate(R.layout.fragment_quality_crashlytics, container, false)
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Crashlytics.setUserIdentifier(Build.FINGERPRINT)
+        forceCrash.setOnClickListener {
+            Crashlytics.getInstance().crash()
+        }
+        createLog.setOnClickListener {
+            Crashlytics.log("Create log was clicked!")
+            Toast.makeText(activity!!, "Crashlytics log was created!", Toast.LENGTH_SHORT).show()
+        }
+        updateKey.setOnClickListener {
+            Crashlytics.setInt("randomNumber", (1..1000).shuffled().last())
+        }
+    }
 }
